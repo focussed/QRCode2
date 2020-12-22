a8fe3bd1bc3d397998ed6e9b8e99490baf9e2b9f
craig.ian@itsligo.ie <craig.ian@itsligo.ie>
2020-12-22 21:46:28 +0000

    Initial commit

---
+++ app/src/main/java/edu/icraig/qrcode2/MainActivity.java
@@ -0,0 +1,65 @@
+package edu.icraig.qrcode2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText etName, etAddress;
+    private IntentIntegrator qrScan = new IntentIntegrator(this);  // ZXing lib linker
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etName = findViewById(R.id.etName);
+        etAddress = findViewById(R.id.etAddress);
+    }
+
+    public void doScan(View view) {
+        qrScan.initiateScan();      // start the QR code scan
+    }
+
+    // add code to process the captured QR Code
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            //if qrcode has nothing in it
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Result Not Found", Toast.LENGTH_LONG).show();
+            } else {
+                //if qr contains data
+                try {
+                    //converting the data to json
+                    JSONObject obj = new JSONObject(result.getContents());
+                    //setting values to textviews
+                    etName.setText(obj.getString("title"));
+                    etAddress.setText(obj.getString("website"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    //if control comes here
+                    //that means the encoded format not matches
+                    //in this case you can display whatever data is available on the qrcode
+                    //to a toast
+                    Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
+                }
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+}
